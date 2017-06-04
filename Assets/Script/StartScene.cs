635e52f33dce2dd4587ec89dc4db423818f28bdb
Jason-Zhao-Jie <261343578@qq.com>
2017-06-04 21:01:39 +0800

    Create Project in Unity 3D

---
+++ Assets/Script/StartScene.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartScene : MonoBehaviour
+{
+    Button btnStart;
+    Button btnLoad;
+    Button btnReadMe;
+
+    // Use this for initialization
+    void Start()
+    {
+        btnStart = GameObject.Find("btnStart").GetComponent<Button>();
+        btnLoad = GameObject.Find("btnLoad").GetComponent<Button>();
+        btnReadMe = GameObject.Find("btnReadMe").GetComponent<Button>();
+
+        btnStart.onClick.AddListener(() =>
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("MainScene");
+        });
+
+        btnLoad.onClick.AddListener(() =>
+        {
+
+        });
+
+        btnReadMe.onClick.AddListener(() =>
+        {
+
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
